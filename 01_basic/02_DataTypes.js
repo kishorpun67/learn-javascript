bb55d51890c4bbc765fb4a9d64f102e62a0c035e
Kishor Pun <71880698+kishorpun67@users.noreply.github.com>
2023-10-16 16:21:30 +0000

    third commit

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,8 @@
+// "use strick";
+
+
+let numberof = "33abc";
+console.log(typeof numberof);
+let converionsNumber = Number(numberof)
+console.log(typeof converionsNumber)
+console.log(converionsNumber)
