3c8581c8210f9a66ae40d7d288cacb2186503415
Kishor Pun <71880698+kishorpun67@users.noreply.github.com>
2023-10-18 21:30:57 +0000

    seven

---
+++ 01_basic/Date.js
@@ -0,0 +1,9 @@
+
+let myDate = new Date;
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date(2023, 5, 23)
+console.log(myCreatedDate.toDateString())
