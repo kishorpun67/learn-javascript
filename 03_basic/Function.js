20be6430cc76af9676543ba8b29e847fcb31c7a4
Kishor Pun <71880698+kishorpun67@users.noreply.github.com>
2023-10-26 14:56:58 +0000

    eleven commit

--- 03_basic/Function.js
+++ 03_basic/Function.js
@@ -1,7 +1,45 @@
-function test(arguments=null) {
-    if(!arguments) { return "none";}
-    return arguments;
+// function test(arguments=null) {
+//     if(!arguments) { return "none";}
+//     return arguments;
+// }
+
+
+// console.log(test())
+
+function test() {
+    let name = "kishor"
+    // console.log(this.name)
+    this.lastName = "pun"
+    console.log(this)
+}
+// test()
+
+
+// function check() {
+//     throw new Error('Error: Parms is required')
+// }
+
+// function printName(name=check()) {
+//     console.log(name)
+// }
+// printName("")
+
+
+let arrowFunction = () =>{
+    let username = "kishor pun"
+    console.log(this)
+    this.email= "test@gmail.com"
+    console.log(this)
 }
 
+arrowFunction()
 
-console.log(test())
+// console.log(typeof arrowFunction)
+
+const addTwoNumber = (num1, num2) => (num1+num2)
+console.log(addTwoNumber(2,5))
+
+
+
+
+
