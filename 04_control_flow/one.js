41493ee3c96b0bcf594cd83dab7973673f65b107
Kishor Pun <71880698+kishorpun67@users.noreply.github.com>
2023-10-27 16:12:20 +0000

    commit

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+let number = 6;
+
+if(number == "6") {
+    console.log('ello')
+}
+
+let agru =1
+switch (agru) {
+    case 1:
+        console.log('sunday')
+        break;
+
+    default:
+        break;
+}
