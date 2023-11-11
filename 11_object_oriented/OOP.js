6b289c116de2cee15c801a3f4fa35c5453da49af
Kishor Pun <71880698+kishorpun67@users.noreply.github.com>
2023-11-11 20:40:44 +0000

    commit

--- 11_object_oriented/OOP.js
+++ 11_object_oriented/OOP.js
@@ -1,12 +1,21 @@
-const user ={
-    name:"kishor",
-    email:"kishorpun55@gmail.com",
+// const user ={
+//     name:"kishor",
+//     email:"kishorpun55@gmail.com",
+//     getUserDetail : ()=>{
+//         console.log(`my name is ${this.name} and email is ${this.email} `)
+//         console.log(this)
+//     }
+// }
+// user.getUserDetail()
 
-    getUserDetail : ()=>{
-        console.log(`my name is ${this.name} and email is ${this.email} `)
-        console.log(this)
-
-    }
-
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+    return this;
 }
-user.getUserDetail()
+
+const userOne = new User("kishor", 12, true)
+const userTwo = new User("Kripesh", 12, true)
+
+console.log(userOne)
